a9e3469dee518ca54ce74e70c320f0e98fd52495
Alzate1075 <edal1075@gmail.com>
2025-05-28 00:27:58 -0500

    Eliminar node_modules del control de versiones

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// filepath: f:\Estudio\Funval\Desarrollo Web\Funval G3\tailwind.config.js
+module.exports = {
+  content: [
+    "./dia-7/proyecto/index.html",
+    // agrega aquí otras rutas si tienes más archivos
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
