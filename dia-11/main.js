202552f67addfc7948a86744824e636d38cd6597
Alzate1075 <edal1075@gmail.com>
2025-06-02 09:53:16 -0500

    Mini-Proyecto

---
+++ dia-11/main.js
@@ -0,0 +1,55 @@
+//Forma de poner comentarios en JS en una sola linea
+/* Otra forma de poner comentarios
+en jS multi-linea */
+
+/*  console.log - LA MANERA DE MOSTRAR COSAS EN CONSOLA */
+
+console.log("Hola mundo");
+
+console.log("2+3");
+
+let var1 = 12;
+console.log(var1);
+
+/* Creación de variables y Constantes */
+//VAR
+//VAR - NO DEBE USARSE por el scope global que tiene.
+
+var pepito = "Hola Pepito";
+console.log(pepito);
+
+//let
+let juanito = "Hola Juanito"; //valor de la variable
+console.log(juanito);
+juanito= "hola juanioto lokillo" //Se puede cambiar el valor de la variable Let
+console.log(juanito);
+juanito= 77;
+console.log(juanito);
+
+//Const
+const pedrito= "hola pedrito";
+console.log(pedrito);
+/* pedrito="hola kevin";  NO SE PUEDE REASIGNAR UNA CONSTANTE!!!*/
+
+/* TIPOS DE DATOS PRIMITIVOS */
+//Tenemos 7 tipos de datos primitivos
+
+/* STRING - TEXTO */
+let nombre = "kevin"; //string o cadena de texto
+let apellido = "Rodriguez Calderon"; //Se toma todos los caracteres incluyendo los espacios
+/* NUMBER */
+
+let enteros = 12; // Tipo enteros
+let decimales = 45.5; //Flotantes o Decimales
+let negativo = -15; // Negativos
+
+/* BOOLEAN */
+let verdadero = true; //O es verdadero
+let falso =false; // o es falso
+
+/* UNDEFINED */
+let indefinida;
+
+/* NULL */
+let kevin = null;
+
