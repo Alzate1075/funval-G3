49280727b65ecd6e8f05a1844dfafaf67e6c1bee
Alzate1075 <edal1075@gmail.com>
2025-06-02 10:20:38 -0500

    Esta si es

--- dia-11/main.js
+++ dia-11/main.js
@@ -31,14 +31,20 @@ const pedrito= "hola pedrito";
 console.log(pedrito);
 /* pedrito="hola kevin";  NO SE PUEDE REASIGNAR UNA CONSTANTE!!!*/
 
+/* LOWER AMEL CASE */
+let apellidoPaterno; //Variable sin espacio pero se diferencia con una Mayuscula en la segunda palabra.
+let apellidoMaterno;
+let apellidoPaternoAbuelo;
+
+
 /* TIPOS DE DATOS PRIMITIVOS */
 //Tenemos 7 tipos de datos primitivos
 
 /* STRING - TEXTO */
 let nombre = "kevin"; //string o cadena de texto
 let apellido = "Rodriguez Calderon"; //Se toma todos los caracteres incluyendo los espacios
-/* NUMBER */
 
+/* NUMBER */
 let enteros = 12; // Tipo enteros
 let decimales = 45.5; //Flotantes o Decimales
 let negativo = -15; // Negativos
@@ -52,4 +58,12 @@ let indefinida;
 
 /* NULL */
 let kevin = null;
+let valor_null = null;
+
+/* SYMBOL */
+let simbolo = Symbol("numerico2");
+
+/* BIG INT */
+let numeroGrande = 21346578393930384774646787829303544573475434672443574004n; //"n" - al final hace la misma funcion que BigInt
+let numeroGrande2 = BigInt(222247589243759072410751475178187185715782137372175); //BigInt para poner números grandes superiores a 64 bits de datos
 
