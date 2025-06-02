e50f5471935573f48415b4e4d4330307d3219921
Alzate1075 <edal1075@gmail.com>
2025-06-02 11:01:59 -0500

    Ojala este si sea

--- dia-11/main.js
+++ dia-11/main.js
@@ -4,7 +4,7 @@ en jS multi-linea */
 
 /*  console.log - LA MANERA DE MOSTRAR COSAS EN CONSOLA */
 
-console.log("Hola mundo");
+console.log("Hola Bogota");
 
 console.log("2+3");
 
@@ -67,3 +67,79 @@ let simbolo = Symbol("numerico2");
 let numeroGrande = 21346578393930384774646787829303544573475434672443574004n; //"n" - al final hace la misma funcion que BigInt
 let numeroGrande2 = BigInt(222247589243759072410751475178187185715782137372175); //BigInt para poner números grandes superiores a 64 bits de datos
 
+console.log(typeof nombre);
+console.log(enteros);
+console.log(johan);
+console.log(simbolo);
+
+/* EJERCICIOS EN CLASE */
+
+// 1. Escribe un comentario en una línea
+
+// 2. Escribe un comentario en varias líneas
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+
+// 4. Imprime por consola el valor de todas las variables
+
+// 5. Imprime por consola el tipo de todas las variables
+
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+
+// 9. A continuación, modifica los valores de las constantes
+
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
+
+
+//1 - Este es el primer punto
+
+/* 2 -  Este es 
+el segundo
+punto */
+
+//3
+let miNombre = "Edwin"; //string
+let miApellido = "Alzate"; //string
+let numeroEntero = 6; // Tipo enteros
+let unDecimal = 35.5; //Flotantes o Decimales
+let negativos = -1; // Negativos
+let verdad = true; //O es verdadero
+let mentira = false; // o es falso
+let niIdea; //indefinido
+let edwin = null;
+let value_raro = null;
+let simboloSistema = Symbol("numerico8");
+let numeroBig = 21346578393930384774646787829303544573475434672443574004n; //"n" - al final hace la misma funcion que BigInt
+let numeroBig2 = BigInt(222247589243759072410751475178187185715782137372175); //BigInt
+
+//4
+console.log(miNombre);
+console.log(miApellido);
+console.log(numeroEnterontero);
+console.log(unDecimal);
+console.log(negativos);
+console.log(verdad);
+console.log(mentira);
+console.log(niIdea)
+console.log(edwin)
+console.log(value_raro)
+console.log(simboloSistema)
+console.log(numeroBig)
+
+//5
+console.log(typeof miNombre);
+console.log(typeof miApellido);
+console.log(typeof numeroEnterontero);
+console.log(typeof unDecimal);
+console.log(typeof negativos);
+console.log(typeof verdad);
+console.log(typeof mentira);
+console.log(typeof miNombre);
+console.log(typeof miNombre);
+console.log(typeof miNombre);
+console.log(typeof miNombre);
+console.log(typeof miNombre);
